e1eddf34bffa3eb6699ee0f2d22f9bc98b59c5c3
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-24 16:40:48 +0300

    lesson-02-01

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,17 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-let isAccess
+let isAccess;
 
 // your code
+isAccess = ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) 
+    console.log(isAccess);
+
+    
+
+
--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,15 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = 1 + Math.floor(Math.random() * 6); // your code
+let dice2 = 1 + Math.floor(Math.random() * 6); // your code
+let isWinningDouble; // your code
 
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
+console.log("Первый бросок: " + dice1);
+console.log("Второй бросок: " + dice2);
 if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+  dice1 > 3 && dice2 > 3;
+  console.log("Выигрышный дубль.");
 } else {
-  console.log('Не выигрышный дубль.')
+  console.log("Не выигрышный дубль.");
 }
-*/
