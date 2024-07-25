316a067b01026d7d67fb1bf29ab33f9fdfc71c73
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-25 23:27:21 +0300

    lessom-02-03

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,38 +4,20 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 39; // тестовое значение, можно изменять
+const score = Math.floor(Math.random() * 101); // тестовое значение, можно изменять
 let grade;
 // your code
 
-// if (grade = score >= 0 && score <= 49) {
-//     console.log('F');
-// } else if (grade = score >= 50 && score <= 69) {
-//     console.log('D');
-// } else if (grade = score >= 70 && score <= 79) {
-//     console.log('C');
-// } else if (grade = score >= 80 && score <= 89) {
-//     console.log('B');
-// } else {
-//     console.log('A');
-// }
-
-switch ((grade = score >= 0 && score <= 49)) {
-  case true:
-    console.log("F");
-    break;
-//   case true:
-//     console.log("D");
-//     break;
-//   case true:
-//     grade = score >= 70 && score <= 79;
-//     console.log("C");
-//     break;
-//   case true:
-//     grade = score >= 80 && score <= 89;
-//     console.log("B");
-//     break;
-//   case true:
-//     grade = score >= 90 && score <= 100;
-//     console.log("A");
+if (grade = score >= 0 && score <= 49) {
+    console.log('F');
+} else if (grade = score >= 50 && score <= 69) {
+    console.log('D');
+} else if (grade = score >= 70 && score <= 79) {
+    console.log('C');
+} else if (grade = score >= 80 && score <= 89) {
+    console.log('B');
+} else {
+    console.log('A');
 }
+
+
