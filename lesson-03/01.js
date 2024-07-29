c9589f85c0fb734a1b64387f791add5696306a08
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-29 21:31:41 +0300

    lesson-03-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,9 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(a, b) {
+  const total = (a + b) / 2;
+  console.log(total);
+  return total;
+}
+
+calculateAverage();
