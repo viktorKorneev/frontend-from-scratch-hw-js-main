d8d460b4c9057e9d251190801001687f705d5ac7
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-29 21:58:22 +0300

    lesson-03-02

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,10 +2,12 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(a) {
+    const total = a
   if (a % 2) {
     console.log(false);
   } else {
     console.log(true);
+    return total
   }
 }
  isEven(20);
