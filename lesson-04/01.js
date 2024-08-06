e1b151991ca9e7fdad236ef0f4a080a2fcea13ea
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-06 22:56:23 +0300

    lesson-04-01

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,16 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+console.log(includesElement([1, 2, 3], 2));
+
+
+
