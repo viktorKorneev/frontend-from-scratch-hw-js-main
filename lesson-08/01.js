d757b9c3471aa56e4409f940f506edc36dc861b9
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-09 22:55:01 +0300

    lesson-08-01

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -15,56 +15,66 @@
 */
 
 const PETS = [
-  { id: 'cat', title: '🐱' },
-  { id: 'dog', title: '🐶' },
-  { id: 'parrot', title: '🦜' },
-  { id: 'fish', title: '🐠' },
-  { id: 'spider', title: '🕷' },
-  { id: 'snake', title: '🐍' },
-  { id: 'hamster', title: '🐹' },
-  { id: 'turtle', title: '🐢' },
-  { id: 'chinchilla', title: '🦇' },
-  { id: 'hedgehog', title: '🦔' },
-  { id: 'rat', title: '🐀' },
-  { id: 'frog', title: '🐸' },
-]
+  { id: "cat", title: "🐱" },
+  { id: "dog", title: "🐶" },
+  { id: "parrot", title: "🦜" },
+  { id: "fish", title: "🐠" },
+  { id: "spider", title: "🕷" },
+  { id: "snake", title: "🐍" },
+  { id: "hamster", title: "🐹" },
+  { id: "turtle", title: "🐢" },
+  { id: "chinchilla", title: "🦇" },
+  { id: "hedgehog", title: "🦔" },
+  { id: "rat", title: "🐀" },
+  { id: "frog", title: "🐸" },
+];
 
-const cart = []
+const cart = [];
 
-const petShop = document.querySelector('.pet-shop')
-const cartList = document.getElementById('cart-list')
-const messageBox = document.getElementById('message-box')
-const clearCartButton = document.getElementById('clear-cart-button')
+const petShop = document.querySelector(".pet-shop");
+const cartList = document.getElementById("cart-list");
+const messageBox = document.getElementById("message-box");
+const clearCartButton = document.getElementById("clear-cart-button");
 
 // Рендерим кнопки для питомцев
 for (let i = 0; i < PETS.length; i++) {
-  const pet = PETS[i]
+  const pet = PETS[i];
 
-  const petButtonElement = document.createElement('button')
-  petButtonElement.classList.add('pet')
-  petButtonElement.id = pet.id
-  petButtonElement.textContent = pet.title
+  const petButtonElement = document.createElement("button");
+  petButtonElement.classList.add("pet");
+  petButtonElement.id = pet.id;
+  petButtonElement.textContent = pet.title;
 
-  petShop.append(petButtonElement)
+  petShop.append(petButtonElement);
 }
 
 // Обновляем отображение корзины
 function updateCartDisplay() {
-  cartList.innerHTML = ''
+  cartList.innerHTML = "";
 
   for (let i = 0; i < cart.length; i++) {
-    const petId = cart[i]
-    const pet = PETS.find((item) => item.id === petId)
-    const petSpanElement = document.createElement('li')
-    petSpanElement.classList.add('pet')
-    petSpanElement.textContent = pet.title
-    cartList.append(petSpanElement)
+    const petId = cart[i];
+    const pet = PETS.find((item) => item.id === petId);
+    const petSpanElement = document.createElement("li");
+    petSpanElement.classList.add("pet");
+    petSpanElement.textContent = pet.title;
+    cartList.append(petSpanElement);
   }
 }
 
-clearCartButton.addEventListener('click', function () {
-  cart.length = 0
-  updateCartDisplay()
-})
+clearCartButton.addEventListener("click", function () {
+  cart.length = 0;
+  updateCartDisplay();
+});
 
 // Твой код:
+petShop.addEventListener("click", function (event) {
+  let target = event.target.id;
+
+  if (cart.length < 3) {
+    cart.push(target);
+    updateCartDisplay();
+  } else {
+    messageBox.textContent = "Вы не можете добавить более 3 питомцев";
+  }
+});
