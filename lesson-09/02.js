3ec45c0b168c26c6ceb3add787303f95fe7db94f
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-17 17:31:43 +0300

    lesson-09-02

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -26,8 +26,8 @@ console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 
 const map = (array, callback) => {
   const newArray = [];
-  array.forEach(el => {
-    newArray.push(callback(el));
+  array.forEach((el, i) => {
+    newArray.push(callback(el, i));
   });
   return newArray;
 };
